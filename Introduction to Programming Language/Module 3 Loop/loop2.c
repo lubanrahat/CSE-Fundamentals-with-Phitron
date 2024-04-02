bd14314ea349f79304c3d937ac58b67adf4886f1
Luban Rahat <153543408+lubanrahat@users.noreply.github.com>
2024-04-03 00:58:36 +0600

    Module 3 loop

---
+++ Introduction to Programming Language/Module 3 Loop/loop2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int i;
+  for(i=1;i<=5;i=i+1) {
+    printf("%d\n",i);
+  }
+
+  printf("\n");
+
+  for(int i=10;i>=1;i--) {
+    printf("%d\n",i);
+  }
+
+  return 0;
+}
---
+++ Introduction to Programming Language/Module 3 Loop/quiz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+  int i = 0; 
+
+  while(i = 0) {
+    printf("Hello\n");
+  }
+  return 0;
+
+  return 0;
+}
