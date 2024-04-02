e3e30d54c21aeeafdaa6dca12d02bd651fe85903
Luban Rahat <153543408+lubanrahat@users.noreply.github.com>
2024-04-02 13:11:24 +0600

    Module 2.5: Practice Day 01

---
+++ Introduction to Programming Language/Module 2.5 Practice Day 01/problem4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+  int n;
+  scanf("%d",&n);
+
+  if(n > 0) {
+    printf("positive\n");
+  }else if(n == 0) {
+    printf("zero\n");
+  }else {
+    printf("negative\n");
+  }
+
+  return 0;
+}
---
+++ Introduction to Programming Language/Module 2.5 Practice Day 01/tempCodeRunnerFile.c
@@ -0,0 +1,1 @@
+printf("Gucci Bag\n");
