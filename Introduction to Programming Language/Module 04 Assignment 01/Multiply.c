1d155f10b4b4fb1b20a9bea384ea842bf672aed8
Luban Rahat <153543408+lubanrahat@users.noreply.github.com>
2024-04-06 14:00:19 +0600

    Module 04 Assignment 01

---
+++ Introduction to Programming Language/Module 04 Assignment 01/Multiply.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+  long long int a,b;
+  scanf("%lld %lld",&a,&b);
+
+  long long int multiplication = a*b;
+
+  printf("%lld\n",multiplication);
+
+  return 0;
+}
