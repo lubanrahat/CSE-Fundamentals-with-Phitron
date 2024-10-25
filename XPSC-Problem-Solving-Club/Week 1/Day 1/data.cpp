fcc2accfd7613b3c49343635b7f3417de47b4f1d
Luban Rahat <lubanrahat@Lubans-Mac-mini.local>
2024-10-26 03:47:27 +0600

    test

---
+++ XPSC-Problem-Solving-Club/Week 1/Day 1/data.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+
+    ios::sync_with_stdio(false),cin.tie(nullptr);
+
+    int n;
+    cin>>n;
+
+    cout<<n*10<<endl;
+
+    return 0;
+}
