09862b6e9e4dfebab8b46165aaf8b63162e09cfe
Luban Rahat <lubanrahat@Lubans-Mac-mini.local>
2024-11-15 19:23:58 +0600

    Week 3 Day 6

---
+++ XPSC-Problem-Solving-Club/Week 3/Day 6/C_Prepend_and_Append.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long i64;
+typedef unsigned long long u64;
+typedef unsigned u32;
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define all(x) (x).begin(),(x).end()
+#define rep(i,n) for(int i = 0; i < n; i++)
+#define per(i,a,n) for (int i = n-1; i >= a; i--)
+#define print(v) for(auto data : v) cout<<data<<" "; cout<<'\n'
+#define tr(it,a) for(auto it = a.begin(); it != a.end(); it++)
+
+void solve() {
+
+    
+
+}
+
+int main() {
+
+    ios::sync_with_stdio(false),cin.tie(nullptr);
+
+    int T;cin>>T;
+
+    while(T--) {
+        solve();
+    }
+
+    return 0;
+}
