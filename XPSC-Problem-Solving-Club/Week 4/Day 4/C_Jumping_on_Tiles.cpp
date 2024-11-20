7c48c832d4eda82bbd9bb1a199ca87d354ae525b
Luban Rahat <lubanrahat@Lubans-Mac-mini.local>
2024-11-20 23:48:23 +0600

    Week 4 Day 4

---
+++ XPSC-Problem-Solving-Club/Week 4/Day 4/C_Jumping_on_Tiles.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) (x).begin(),(x).end()
+
+int main() {
+
+    ios::sync_with_stdio(false),cin.tie(nullptr);
+
+    
+
+    return 0;
+}
